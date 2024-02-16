4e5a3b7f2aa6b59a7b7148b72f4395e7f941a5ca
big-good-boy <drago999@bk.ru>
2024-02-16 16:41:12 +0500

    Создание фильтра вынесено отдельно

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import {createSiteMenuTemplate} from "./components/site-menu.js";
 import {createSortingTemplate} from "./components/sorting.js";
 import {createTaskTemplate} from "./components/task.js";
 import {createTaskEditTemplate} from "./components/task-edit.js";
+import {generateFilters} from './mock/filter.js';
 
 const TASK_COUNT = 3;
 
@@ -16,7 +17,7 @@ const siteMainElement = document.querySelector(`.main`);
 const siteHeaderElement = siteMainElement.querySelector(`.main__control`);
 
 render(siteHeaderElement, createSiteMenuTemplate());
-render(siteMainElement, createFilterTemplate());
+render(siteMainElement, createFilterTemplate(generateFilters()));
 render(siteMainElement, createBoardTemplate());
 
 const boardElement = siteMainElement.querySelector(`.board`);

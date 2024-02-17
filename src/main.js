3568faa7bea4e6a876bab4a67ff556c7a6ef22c1
big-good-boy <drago999@bk.ru>
2024-02-17 16:10:06 +0500

    Генерация указанного количества карточек

--- src/main.js
+++ src/main.js
@@ -2,13 +2,15 @@ import {createBoardTemplate} from "./components/board.js";
 import {createFilterTemplate} from "./components/filter.js";
 import {createLoadMoreButtonTemplate} from "./components/load-more-button.js";
 import {createSiteMenuTemplate} from "./components/site-menu.js";
-import {createSortingTemplate} from "./components/sorting.js";
-import {createTaskTemplate} from "./components/task.js";
+import {createSortingTemplate} from './components/sorting.js';
 import {createTaskEditTemplate} from "./components/task-edit.js";
+import {createTaskTemplate} from './components/task.js';
 import {generateFilters} from './mock/filter.js';
 import {generateTasks} from './mock/task.js';
 
-const TASK_COUNT = 3;
+const TASK_COUNT = 22;
+const SHOWING_TASKS_COUNT_ON_START = 8;
+const SHOWING_TASKS_COUNT_BY_BUTTON = 8;
 
 const render = (container, template, place = `beforeEnd`) => {
   container.insertAdjacentHTML(place, template);
@@ -30,8 +32,24 @@ render(boardElement, createSortingTemplate(), `afterbegin`);
 
 render(taskListElement, createTaskEditTemplate(tasks[0]));
 
-for (let i = 0; i < tasks.length; i++) {
-  render(taskListElement, createTaskTemplate(tasks[i]));
-}
+let showingTasksCount = SHOWING_TASKS_COUNT_ON_START;
+tasks
+  .slice(1, showingTasksCount)
+  .forEach((task) => render(taskListElement, createTaskTemplate(task)));
 
 render(boardElement, createLoadMoreButtonTemplate());
+
+const loadMoreButton = boardElement.querySelector(`.load-more`);
+
+loadMoreButton.addEventListener(`click`, () => {
+  const prevTaskCount = showingTasksCount;
+  showingTasksCount = showingTasksCount + SHOWING_TASKS_COUNT_BY_BUTTON;
+
+  tasks
+    .slice(prevTaskCount, showingTasksCount)
+    .forEach((task) => render(taskListElement, createTaskTemplate(task)));
+
+  if (showingTasksCount >= tasks.length) {
+    loadMoreButton.remove();
+  }
+});

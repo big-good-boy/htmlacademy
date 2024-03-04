81592ba3373da1a164bf478a61817cac039254a8
big-good-boy <drago999@bk.ru>
2024-03-04 16:24:54 +0500

    Добавлен класс карточки

--- src/components/task.js
+++ src/components/task.js
@@ -1,7 +1,7 @@
 import {MONTH_NAMES} from '../const';
-import {formatTime} from '../utils';
+import {createElement, formatTime} from '../utils';
 
-export const createTaskTemplate = (task) => {
+const createTaskTemplate = (task) => {
   const {description, dueDate, color, repeatingDays, isArchive, isFavorite} =
     task;
 
@@ -64,3 +64,26 @@ export const createTaskTemplate = (task) => {
     </div>
   </article>`;
 };
+
+export default class Task {
+  constructor(task) {
+    this._task = task;
+    this._element = null;
+  }
+
+  getTemplate() {
+    return createTaskTemplate(this._task);
+  }
+
+  getElement() {
+    if (!this._element) {
+      this._element = createElement(this.getTemplate());
+    }
+
+    return this._element;
+  }
+
+  removeElement() {
+    this._element = null;
+  }
+}

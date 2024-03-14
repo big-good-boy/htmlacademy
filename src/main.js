8f2332d15a9bb2140eb74f7e12594c1d89836f65
big-good-boy <drago999@bk.ru>
2024-03-14 13:01:52 +0500

    Отрисовка задач вынесена в отдельные функции

--- src/main.js
+++ src/main.js
@@ -14,40 +14,23 @@ const TASK_COUNT = 22;
 const SHOWING_TASKS_COUNT_ON_START = 8;
 const SHOWING_TASKS_COUNT_BY_BUTTON = 8;
 
+const renderTask = () => {};
+
+const renderBoard = () => {};
+
 const siteMainElement = document.querySelector(`.main`);
 const siteHeaderElement = siteMainElement.querySelector(`.main__control`);
 
 const filters = generateFilters();
 const tasks = generateTasks(TASK_COUNT);
 
-render(siteHeaderElement, createSiteMenuTemplate());
-render(siteMainElement, createFilterTemplate(filters));
-render(siteMainElement, createBoardTemplate());
-
-const boardElement = siteMainElement.querySelector(`.board`);
-const taskListElement = siteMainElement.querySelector(`.board__tasks`);
-render(boardElement, createSortingTemplate(), `afterbegin`);
-
-render(taskListElement, createTaskEditTemplate(tasks[0]));
-
-let showingTasksCount = SHOWING_TASKS_COUNT_ON_START;
-tasks
-  .slice(1, showingTasksCount)
-  .forEach((task) => render(taskListElement, createTaskTemplate(task)));
-
-render(boardElement, createLoadMoreButtonTemplate());
-
-const loadMoreButton = boardElement.querySelector(`.load-more`);
-
-loadMoreButton.addEventListener(`click`, () => {
-  const prevTaskCount = showingTasksCount;
-  showingTasksCount = showingTasksCount + SHOWING_TASKS_COUNT_BY_BUTTON;
-
-  tasks
-    .slice(prevTaskCount, showingTasksCount)
-    .forEach((task) => render(taskListElement, createTaskTemplate(task)));
-
-  if (showingTasksCount >= tasks.length) {
-    loadMoreButton.remove();
-  }
-});
+render(
+    siteHeaderElement,
+    new SiteMenuComponent().getElement(),
+    RenderPosition.BEFOREEND
+);
+render(
+    siteMainElement,
+    new FilterComponent(filters).getElement(),
+    RenderPosition.BEFOREEND
+);

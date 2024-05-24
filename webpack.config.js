0fcf6b1e08b27ab51d889d26c9db12edf0806fb5
big-good-boy <drago999@bk.ru>
2024-05-24 12:05:58 +0500

    Установлен и настроен flatpickr

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,19 @@
 const path = require(`path`);
 
 module.exports = {
-	mode: `development`,
-	entry: `./src/main.js`,
-	output: {
-		filename: `bundle.js`,
-		path: path.join(__dirname, `public`),
-	},
-	devtool: `source-map`
+  mode: `development`,
+  entry: `./src/main.js`,
+  output: {
+    filename: `bundle.js`,
+    path: path.join(__dirname, `public`),
+  },
+  devtool: `source-map`,
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
 };
